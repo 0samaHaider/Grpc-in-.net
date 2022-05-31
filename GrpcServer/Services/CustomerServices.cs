30bed66a878001a31434c4192f4b1a6bad0a4e08
osama.haider <osama.haider@TEO-OSAMAH>
2022-05-31 15:27:27 +0500

    Add project files.

---
+++ GrpcServer/Services/CustomerServices.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CustomerServices :Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerServices> _logger;
+        public CustomerServices(ILogger<CustomerServices> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            if (request.UserId == 1)
+            {
+                output.FirstName = "Jamie";
+                output.LastName = "Smith";
+            }
+            else if (request.UserId == 2)
+            {
+                output.FirstName = "Jane";
+                output.LastName = "Doe";
+            }
+            else
+            {
+                output.FirstName = "Greg";
+                output.LastName = "Thomas";
+            }
+
+            return Task.FromResult(output);
+        }
+    }
+}
---
+++ Grpc_Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var input = new HelloRequest { Name = "Osama" };
+
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+
+            //var reply = await client.SayHelloAsync(input);
+
+            //Console.WriteLine(reply.Message);
+
+
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+#pragma warning disable CS0436 // Type conflicts with imported type
+            var customerClient = new Customer.CustomerClient(channel);
+#pragma warning restore CS0436 // Type conflicts with imported type
+
+            int userId;
+            userId =Int32.Parse(Console.ReadLine());
+
+#pragma warning disable CS0436 // Type conflicts with imported type
+            var clientRequested = new CustomerLookupModel { UserId = userId };
+#pragma warning restore CS0436 // Type conflicts with imported type
+
+            var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
+
+            Console.WriteLine($"{ customer.FirstName } { customer.LastName }");
+            Console.ReadLine();
+        }
+    }
+}
